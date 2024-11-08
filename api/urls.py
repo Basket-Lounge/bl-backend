41d4be5d302ff98f3f778d0335658af6f8c05b03
Ud293nBqda@outlook.kr <johndoe@example.com>
2024-11-09 02:14:35 +0900

    Feat: 문의 관련 라우터 엔트리 추가

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from games.views import GameViewSet
+from management.views import InquiryViewSet
 from players.views import PlayersViewSet
 from rest_framework.routers import DefaultRouter
 
@@ -9,6 +10,7 @@ from users.views import JWTViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register(r'games', GameViewSet, basename='game')
+router.register(r'inquiries', InquiryViewSet, basename='inquiry')
 router.register(r'players', PlayersViewSet, basename='player')
 router.register(r'teams', TeamViewSet, basename='team')
 router.register(r'teams/posts', TeamsPostViewSet, basename='post')
