57e1808354cca4ec3e85307574ea5f1a6a69d8f4
Ud293nBqda@outlook.kr <johndoe@example.com>
2025-01-18 02:06:25 +0900

    Feat: A new task function added for live inquiry update

--- management/tasks.py
+++ management/tasks.py
@@ -1,30 +1,42 @@
 from celery import shared_task
 
-from management.models import InquiryModeratorMessage
-from management.services.models_services import filter_and_fetch_inquiry
+from management.services.models_services import (
+    InquiryModeratorService, 
+    filter_and_fetch_inquiry
+)
 from management.services.serializers_services import (
     send_inquiry_message_to_live_chat, 
     send_inquiry_notification_to_all_channels_for_moderators, 
     send_inquiry_notification_to_specific_moderator, 
-    send_inquiry_notification_to_user
+    send_inquiry_notification_to_user,
+    send_partially_updated_inquiry_to_live_chat
 )
 
 @shared_task
-def broadcast_inquiry_updates_to_all_parties(inquiry_id, message_id):
-    message = InquiryModeratorMessage.objects.get(id=message_id)
+def broadcast_inquiry_updates_for_new_message_to_all_parties(inquiry_id, message_id):
+    message = InquiryModeratorService.get_inquiry_moderator_message(message_id)
     inquiry = filter_and_fetch_inquiry(id=inquiry_id)
 
     send_inquiry_message_to_live_chat(message, inquiry.id)
-    send_inquiry_notification_to_user(
-        inquiry,
-        inquiry.user.id,
-        inquiry.last_read_at
-    )
+    send_inquiry_notification_to_user(inquiry)
     send_inquiry_notification_to_all_channels_for_moderators(inquiry)
 
     for moderator in inquiry.inquirymoderator_set.all():
         send_inquiry_notification_to_specific_moderator(
             inquiry,
             moderator.moderator.id,
-            moderator.last_read_at
+        )
+
+@shared_task
+def broadcast_inquiry_updates_to_all_parties(inquiry_id):
+    inquiry = filter_and_fetch_inquiry(id=inquiry_id)
+
+    send_partially_updated_inquiry_to_live_chat(inquiry)
+    send_inquiry_notification_to_user(inquiry)
+    send_inquiry_notification_to_all_channels_for_moderators(inquiry)
+
+    for moderator in inquiry.inquirymoderator_set.all():
+        send_inquiry_notification_to_specific_moderator(
+            inquiry,
+            moderator.moderator.id,
         )
